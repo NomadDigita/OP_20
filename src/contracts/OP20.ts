85985007265e5d091a6879a85ac5b87fe9223786
Asiwaju <abdulhameedmuhammadthalith@gmail.com>
2025-05-10 00:32:51 +0100

    Create OP20.ts

---
+++ src/contracts/OP20.ts
@@ -0,0 +1,6 @@
+// src/contracts/OP20.ts
+import { DeployableOP_20 } from '@btc-vision/btc-runtime';
+
+export class OP20 extends DeployableOP_20 {
+  // Define any necessary logic for your OP_20 token
+}
