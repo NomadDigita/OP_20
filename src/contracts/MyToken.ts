e6ab12886ca54687747617727cb20b4eb6d0dfa5
Asiwaju <abdulhameedmuhammadthalith@gmail.com>
2025-05-09 23:58:58 +0100

    Update MyToken.ts

--- src/contracts/MyToken.ts
+++ src/contracts/MyToken.ts
@@ -1,7 +1,9 @@
-import { OP20 } from "../OP20";
+import { OP20 } from "./OP20";  // Adjust path based on actual location
+import { u128, u256 } from '@btc-vision/btc-runtime/runtime/utils/int';
 
 export class MyToken extends OP20 {
-  constructor() {
-    super("FurWhisk", "FUR", 18, 1000000000000000000000000); // 1M with 18 decimals
-  }
+    constructor() {
+        // Use u128 and u256 to handle large number literals
+        super("FurWhisk", "FUR", 18, u128.fromString("1000000000000000000000000").toU256());
+    }
 }
